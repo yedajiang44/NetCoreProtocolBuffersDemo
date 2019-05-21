8da8281bb15dedb8e0e4a9614206d2324bc10c08
yedajiang44 <602830483@qq.com>
2019-05-21 22:21:04 +0800

    添加项目文件。

---
+++ Protocol Buffers Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProtocolBuffersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yedajing44 = new Person
+            {
+                Id = 1234,
+                Name = "yedajiang44",
+                Email = "yedajiang44@163.com",
+                Phones = {
+                    new Person.Types.PhoneNumber { Number = "123456789", Type = Person.Types.PhoneType.Home },
+                    new Person.Types.PhoneNumber { Number = "987654321", Type = Person.Types.PhoneType.Work }
+                }
+            };
+            var json = yedajing44.ToString();
+            Console.WriteLine($"序列化：{json}");
+            yedajing44 = Person.Parser.ParseJson(json);
+            Console.WriteLine($"反序列化{yedajing44}");
+            Console.ReadKey();
+        }
+    }
+}
